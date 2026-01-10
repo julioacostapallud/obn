7b23f4a1156770938da09f99a099767945f9aefb
Julio Acosta <julioacostapallud@gmail.com>
2026-01-10 00:46:22 -0300

    Fix: eliminar import no utilizado useRef

--- src/components/MedievalIntro.tsx
+++ src/components/MedievalIntro.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import './MedievalIntro.css';
 import { medievalMusic } from '../utils/medievalMusic';
 
